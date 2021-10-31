5aa8ad3c94ab0a3d4a111a22b4e2723191c3eff0
Moonrise <popovleonid105@gmail.com>
2021-10-31 16:28:56 +0300

    ign

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop',
         'USER': 'moonrise',
-        'PASSWORD': 'popov105',
+        'PASSWORD': 'popov1051',
         'HOST': 'localhost',
         'PORT': '',
     }
